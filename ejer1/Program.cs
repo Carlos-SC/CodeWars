34eb6ea4b66fb94f8b8b07b1af6ec387542ede29
DAM228@ACARBALLEIRA.LOCAL <DAM228@ACARBALLEIRA.LOCAL>
2021-09-27 13:22:17 +0200

    Agregar archivos de proyecto.

---
+++ ejer1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
